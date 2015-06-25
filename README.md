61ad40603bcbcda9f0febe0b543bd3227a662532
pman-emp <p.manchev@emerchantpay.com>
2015-06-25 17:27:16 +0300

    1.2.1

--- README.md
+++ README.md
@@ -1,60 +1,47 @@
 Genesis client for Magento CE
 =============================
 
-This is a Payment Module for E-ComProcessing that gives you the ability to process payments through E-ComProcessing's Payment Gateway - Genesis from within your Magento-based Store.
+This is a Payment Module for Magento Community Edition, that gives you the ability to process payments through E-ComProcessing's Payment Gateway - Genesis.
 
 Requirements
 ------------
 
-* Magento CE (Community Edition) >= 1.7
-* GenesisPHP 1.0.1
+* Magento Community Edition* > 1.7
+* GenesisPHP 1.2
+* PCI-certified server in order to use ```E-ComProcessing Direct```
 
-Note: this module has been tested only with Magento Community Edition, it may not work
-as intended with Magento Enterprise Edition
+*Note: this module has been tested only with Magento __Community Edition__, it may not work
+as intended with Magento __Enterprise Edition__
 
 GenesisPHP Requirements
 ------------
 
-* PHP >= 5.3 (built w/ libxml)
-* PHP Extensions: cURL (optionally you can use Streams, but its not recommended on PHP < 5.6)
-* Composer
+* PHP version 5.3.2 or newer
+* PHP Extensions:
+    * [BCMath](https://php.net/bcmath)
+    * [CURL](https://php.net/curl) (required, only if you use the curl network interface)
+    * [Filter](https://php.net/filter)
+    * [Hash](https://php.net/hash)
+    * [XMLReader](https://php.net/xmlreader)
+    * [XMLWriter](https://php.net/xmlwriter)
 
-
-Installation (auto)
+Installation (via Modman)
 ---------------------
 
 * Install [ModMan]
 * Navigate to the root of your Magento installation
-* run `modman init`
-* and clone this repo `modman clone https://github.com/E-ComProcessing/magento-ce-ecp-plugin`
-* Flush all Caches (from System -> Cache Management)
-* Login inside the Admin Panel and go to System -> Configuration -> Payment Methods
-* Check "Enable" and set the correct credentials, select your preferred payment method and click "Save config"
-
-You're now ready to process payments through our gateway.
-
+* run ```modman init```
+* and clone this repo ```modman clone https://github.com/E-ComProcessing/magento-ce-ecp-plugin```
+* Login inside the Admin Panel and go to ```System``` -> ```Configuration``` -> ```Payment Methods```
+* Check ```Enable```, set the correct credentials, select your prefered payment method and click ```Save config```
 
 Installation (manual)
 ---------------------
 
 * Copy the files to the root folder of your Magento installation
-* Flush all Caches (from System -> Cache Management)
-* Login inside the Admin Panel and go to System -> Configuration -> Payment Methods
-* Check "Enable" and set the correct credentials, select your prefered payment method and click "Save config"
+* Login inside the Admin Panel and go to ```System``` -> ```Configuration``` -> ```Payment Methods```
+* Check ```Enable```, set the correct credentials, select your prefered payment method and click ```Save config```
 
 You're now ready to process payments through our gateway.
-
-Supported Transaction Types
----------------------------
-
-Currently, we support the following transaction types (for more in-depth information about these transaction types, you can reference the Genesis API Documentation):
-
-* Authorize
-* Authorize with 3D-Secure
-* Capture
-* Sale (Auth&Capture)
-* Sale (Auth&Capture) with 3D-Secure
-* Refund
-* Void
 
 [ModMan]: https://github.com/colinmollenhour/modman
--- lib/Genesis/src/Genesis/API/Response.php
+++ lib/Genesis/src/Genesis/API/Response.php
@@ -1,16 +1,33 @@
 <?php
-/**
- * Response handler - handles responses from Genesis Gateway
+/*
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- * @package Genesis
- * @subpackage API
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @license     http://opensource.org/licenses/MIT The MIT License
  */
-
 namespace Genesis\API;
 
-use Genesis\Exceptions;
-use Genesis\Network\Request as Network;
-
+/**
+ * Response - process/format an incoming Genesis response
+ *
+ * @package    Genesis
+ * @subpackage API
+ */
 class Response
 {
     /**
@@ -28,19 +45,73 @@ class Response
     public $responseRaw;
 
     /**
-     * Network Context Handle
+     * Genesis Request Context
      *
-     * @var Network
+     * @var \Genesis\API\Request
      */
-    private $networkContext;
+    protected $requestCtx;
 
-    public function __construct(Network $networkContext)
+    /**
+     * Initialize with NetworkContext (if available)
+     *
+     * @param \Genesis\Network|null $networkContext
+     *
+     * @throws \Genesis\Exceptions\InvalidArgument
+     */
+    public function __construct($networkContext = null)
     {
-        $this->networkContext = $networkContext;
-
-        if ( strlen($this->networkContext->getResponseBody()) > 0 ) {
-            $this->parseResponse($this->networkContext->getResponseBody());
+        if (!is_null($networkContext) && is_a($networkContext, '\Genesis\Network')) {
+            $this->parseResponse($networkContext->getResponseBody());
         }
+    }
+
+    /**
+     * Parse Genesis response to stdClass and
+     * apply transformation to known fields
+     *
+     * @param string $response
+     *
+     * @throws \Genesis\Exceptions\ErrorAPI
+     * @throws \Genesis\Exceptions\InvalidArgument
+     * @throws \Genesis\Exceptions\InvalidResponse
+     */
+    public function parseResponse($response)
+    {
+        $this->responseRaw = $response;
+
+        try {
+            $parser = new \Genesis\Parser('xml');
+            $parser->skipRootNode();
+            $parser->parseDocument($response);
+
+            $this->responseObj = $parser->getObject();
+        } catch (\Exception $e) {
+            throw new \Genesis\Exceptions\InvalidResponse(
+                $e->getMessage(),
+                $e->getCode()
+            );
+        }
+
+        if (isset($this->responseObj->status)) {
+            $state = new Constants\Transaction\States($this->responseObj->status);
+
+            if (!$state->isValid()) {
+                throw new \Genesis\Exceptions\InvalidArgument(
+                    'Unknown transaction status',
+                    isset($this->responseObj->code) ? $this->responseObj->code : 0
+                );
+            }
+
+            if ($state->isError() && !$this->suppressReconciliationException()) {
+                throw new \Genesis\Exceptions\ErrorAPI(
+                    $this->responseObj->message,
+                    isset($this->responseObj->code) ? $this->responseObj->code : 0
+                );
+            }
+        }
+
+        // Apply per-field transformations
+        $this->transform(array($this->responseObj));
     }
 
     /**
@@ -49,32 +120,85 @@ class Response
      * Note: You should consult with the documentation
      * which transaction responses have status available.
      *
-     * @return bool
+     * @return bool | null (on missing status)
      */
     public function isSuccessful()
     {
-        $status = false;
+        $status = new Constants\Transaction\States(
+            isset($this->responseObj->status) ? $this->responseObj->status : ''
+        );
 
-        if (isset($this->responseObj->status) && in_array($this->responseObj->status, array('approved', 'pending_async'))) {
-            $status = true;
+        if ($status->isValid()) {
+            if ($status->isError()) {
+                $result = false;
+            } else {
+                $result = true;
+            }
+        } else {
+            // return null if status is inapplicable
+            $result = null;
         }
 
-        return $status;
+        return $result;
+    }
+
+    /**
+     * Check whether the transaction was partially approved
+     *
+     * @see Genesis_API_Documentation for more information
+     *
+     * @return bool | null (if inapplicable)
+     */
+    public function isPartiallyApproved()
+    {
+        if (isset($this->responseObj->partial_approval)) {
+            return \Genesis\Utils\Common::stringToBoolean($this->responseObj->partial_approval);
+        }
+
+        return null;
+    }
+
+    /**
+     * Suppress Reconciliation responses as their statuses
+     * reflect their transactions
+     *
+     * @return bool
+     */
+    public function suppressReconciliationException()
+    {
+        $instances = array(
+            new \Genesis\API\Request\NonFinancial\Reconcile\DateRange(),
+            new \Genesis\API\Request\NonFinancial\Reconcile\Transaction(),
+            new \Genesis\API\Request\WPF\Reconcile()
+        );
+
+        if (isset($this->requestCtx) && isset($this->responseObj->unique_id)) {
+            foreach ($instances as $instance) {
+                if ($this->requestCtx instanceof $instance) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 
     /**
      * Try to fetch a description of the received Error Code
      *
-     * @return bool|string
+     * @return string | null (if inapplicable)
      */
     public function getErrorDescription()
     {
-        if (isset($this->responseObj->code)) {
-            return Errors::getErrorDescription($this->responseObj->code);
+        if (isset($this->responseObj->code) && !empty($this->responseObj->code)) {
+            return Constants\Errors::getErrorDescription($this->responseObj->code);
         }
-        else {
-            return Errors::getIssuerResponseCode($this->responseObj->response_code);
+
+        if (isset($this->responseObj->response_code) && !empty($this->responseObj->response_code)) {
+            return Constants\Errors::getIssuerResponseCode($this->responseObj->response_code);
         }
+
+        return null;
     }
 
     /**
@@ -98,18 +222,96 @@ class Response
     }
 
     /**
-     * Parse Genesis response to SimpleXMLElement
+     * Set Genesis Request context
      *
-     * @param $response \SimpleXMLElement
-     * @throws \Genesis\Exceptions\InvalidArgument
+     * @param $requestCtx
      */
-    public function parseResponse($response)
+    public function setRequestCtx($requestCtx)
     {
-        if (empty($response)) {
-            throw new Exceptions\InvalidArgument();
+        $this->requestCtx = $requestCtx;
+    }
+
+    /**
+     * Iterate and transform object
+     *
+     * @param mixed $obj
+     */
+    public static function transform($obj)
+    {
+        if (is_array($obj) || is_object($obj)) {
+            foreach ($obj as &$object) {
+                if (isset($object->status)) {
+                    self::transformObject($object);
+                }
+
+                self::transform($object);
+            }
+        }
+    }
+
+    /**
+     * Apply filters to an entry object
+     *
+     * @param \stdClass|\ArrayObject $entry
+     *
+     * @return mixed
+     */
+    public static function transformObject(&$entry)
+    {
+        $filters = array(
+            'transformFilterAmount',
+            'transformFilterTimestamp'
+        );
+
+        foreach ($filters as $filter) {
+            if (method_exists(__CLASS__, $filter)) {
+                $result = call_user_func(array(__CLASS__, $filter), $entry);
+
+                if ($result) {
+                    $entry = $result;
+                }
+            }
+        }
+    }
+
+    /**
+     * Get formatted amount (instead of ISO4217, return in float)
+     *
+     * @param \stdClass|\ArrayObject $transaction
+     *
+     * @return String | null (if amount/currency are unavailable)
+     */
+    public static function transformFilterAmount($transaction)
+    {
+        // Process a single transaction
+        if (isset($transaction->currency) && isset($transaction->amount)) {
+            $transaction->amount = \Genesis\Utils\Currency::exponentToAmount(
+                $transaction->amount,
+                $transaction->currency
+            );
         }
 
-        $this->responseRaw = $response;
-        $this->responseObj = simplexml_load_string($response);
+        return $transaction;
+    }
+
+    /**
+     * Get DateTime object from the timestamp inside the response
+     *
+     * @param \stdClass|\ArrayObject $transaction
+     *
+     * @return \DateTime|null (if invalid timestamp)
+     */
+    public static function transformFilterTimestamp($transaction)
+    {
+        if (isset($transaction->timestamp)) {
+            try {
+                $transaction->timestamp = new \DateTime($transaction->timestamp);
+            } catch (\Exception $e) {
+                // Just log the attempt
+                error_log($e->getMessage());
+            }
+        }
+
+        return $transaction;
     }
 }
